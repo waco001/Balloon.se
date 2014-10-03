c01bac6d1db50109ae4affcb7b4263a6f404ef29
waco001 <abhishek.gorti@gmail.com>
2014-10-02 22:06:22 -0400

    duplicate

--- API/User.py
+++ API/User.py
@@ -5,7 +5,6 @@ class UserRegister(Resource):
 		parser = reqparse.RequestParser()
         parser.add_argument('phone_number', type=int, required=True)
       	parser.add_argument('provider', type=str, required=True)
-      	parser.add_argument('provider', type=str, required=True)
         request_params = parser.parse_args()
         result = process_the_request(request_params)
         response = {
