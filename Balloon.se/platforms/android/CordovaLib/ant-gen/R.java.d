9778e97cf2ff0e375fb83a8061b5050d470e0247
waco001 <waco001@Abhisheks-MacBook-Air.local>
2014-10-07 11:18:38 -0400

    New Phonegap Application

--- Balloon.se/platforms/android/CordovaLib/ant-gen/R.java.d
+++
@@ -1,1 +0,0 @@
- : /Users/waco001/Desktop/Balloon.se/Balloon.se/platforms/android/CordovaLib/ant-build/AndroidManifest.xml \
--- Balloon.se/platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/waco001/Desktop/Balloon.se/Balloon.se/platforms/android/ant-build/AndroidManifest.xml : \
-/Users/waco001/Desktop/Balloon.se/Balloon.se/platforms/android/AndroidManifest.xml \
